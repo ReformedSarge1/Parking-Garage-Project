5517a53566cb9f6a9fb394c4336265419dfc0257
Carter Cook <raymanomen3014@gmail.com>
2022-10-02 17:48:23 -0700

    Carter's Second Commit

--- Garage-Project.py
+++ Garage-Project.py
@@ -1,15 +1,16 @@
 import random
 
 class ParkingGarage():
-    def __init__(self):#, toPay, ticket, space):
+    def __init__(self):
         self.tickets = [1,2,3,4,5,6,7,8,9]
         self.ticket_number = 9
         self.parkingSpaces = [1,2,3,4,5,6,7,8,9]
         self.currentTicket = {}
         self.tix_space_paid = []
-        # self.toPay = toPay
-        # self.ticket = ticket
-        # self.space = space
+        self.rate = 5.0
+        self.paid = False
+         
+        
 
     def takeTicket(self):
         while True:
@@ -20,10 +21,11 @@ class ParkingGarage():
                 else:
                     random_space = random.randint(1,9)
                     self.parkingSpaces.remove(random_space)
+                    self.tickets.pop(0)
                     self.tix_space_paid.append(self.tickets[0])
-                    self.tix_space_paid.append(False)
+                    self.tix_space_paid.append(self.paid)
                     self.currentTicket = {random_space:self.tix_space_paid}
-                    print(f'Here is your ticket. Please park in space {random_space}.')
+                    print(f'Your rate is $5 an hour, Here is your ticket. Please park in space {random_space}.')
                     return
             elif welcome == 'n':
                 print('Thanks for considering parking in our lot. Have a great day!')
@@ -32,10 +34,28 @@ class ParkingGarage():
                 print('Please type \'y\' or \'n\'')
 
 
-    def payForParking(self):
-        self.toPay = input("How much you pay")
+    def payForParking(self,time,space):
+        self.time = time
+        self.space = space
+        if self.time < 0.25: 
+            print("You have been here for 15 minutes, you do not have to pay. Thank you for choosing CartBo Parking.")
+        else:
+            print(f'Your cost to park is ${"{:.2f}".format(self.time * self.rate)}.')
+            space = input(f"Which space were you parked in?")
+            self.tix_space_paid[1] = True           #Self.paid = True. 
+            self.currentTicket = {space:self.tix_space_paid}
+            self.parkingSpaces.append(int(space))      #adding space back into space list.
+            self.ticket_number += 1
+            self.tickets.append(self.ticket_number)
 
-    # def leaveGarage(self):
+
+    def leaveGarage(self): 
+        if self.currentTicket[self.space]:
+            print("Thank you for paying, you have 15 minutes to leave.")
+
+
+
 
 pg = ParkingGarage()
 pg.takeTicket()
+pg.payForParking(7.5)
